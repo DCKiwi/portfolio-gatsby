c04faffc8c07210a051712f320a5e9017bdfacc7
Your NameDavid <davidcnz@gmail.com>
2019-02-04 20:33:28 -0700

    intial init

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About</h1>
+      <p>This is all about me!</p>
+    </div>
+  </Layout>
+);
+
+export default About;
